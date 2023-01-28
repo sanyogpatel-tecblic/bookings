0a597273532bfed70ec667f1814bbfb51667c57f
sanyogpatel-tecblic <sanyogpatel2491999@gmail.com>
2023-01-28 18:16:06 +0530

    add

---
+++ internal/repository/repository.go
@@ -0,0 +1,20 @@
+package repository
+
+import (
+	"time"
+
+	"github.com/sanyogpatel-tecblic/bookings/internal/models"
+)
+
+type DatabaseRepo interface {
+	AllUsers() bool
+
+	InsertReservation(res models.Reservation) (int, error)
+	InsertRoomRestriction(r models.RoomRestriction) error
+	SearchAvailabilityByDatesByRoomID(start, end time.Time, roomID int) (bool, error)
+	SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error)
+	GetRoomByID(id int) (models.Room, error)
+	GetUserByID(id int) (models.User, error)
+	UpdateUser(user models.User) error
+	Authenticate(email, testPassword string) (int, string, error)
+}
